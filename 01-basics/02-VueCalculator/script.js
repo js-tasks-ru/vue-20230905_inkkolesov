7754f4b169f6366327b29e560cd9f764488f6a50
inkoff <kob872list.ru>
2023-09-12 11:36:46 +0300

    решение по калькулятору

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,27 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      number_1: 0,
+      number_2: 0,
+      operator: '',
+    };
+  },
+  computed:{
+    calculatedOutput(){
+      switch (this.operator) {
+        case "sum":
+          return this.number_1 + this.number_2;
+        case "subtract":
+          return this.number_1 - this.number_2;
+        case "multiply":
+          return this.number_1 * this.number_2;
+        case "divide":
+          return this.number_1 / this.number_2;
+        default:
+          return 0;
+      }
+    },
+  },
+}).mount('#app')
