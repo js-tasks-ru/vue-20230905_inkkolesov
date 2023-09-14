0f02ce6145e191d3b0e1cc43a4d6b3c6ab1de865
inkoff <kob872list.ru>
2023-09-14 19:04:55 +0300

    решение PageMeetup

---
+++ 02-components/07-PageMeetup/components/MeetupAgendaItem.js
@@ -0,0 +1,43 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from '../meetupService';
+
+export default defineComponent({
+  name: 'MeetupAgendaItem',
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    agendaItemIconsType(){
+      return agendaItemIcons[this.agendaItem.type]
+    },
+    agendaItemDefaultTitlesType(){
+
+      return this.agendaItem.title ? this.agendaItem.title : agendaItemDefaultTitles[this.agendaItem.type]
+    },
+    agendaItemTime(){
+      return this.agendaItem.startsAt + ' - ' + this.agendaItem.endsAt
+    },
+  },
+
+  template: `
+    <div class="agenda-item">
+      <div class="agenda-item__col">
+        <img :src="\`/assets/icons/icon-\${agendaItemIconsType}.svg\`" class="icon" :alt="agendaItemIconsType" />
+      </div>
+      <div class="agenda-item__col">{{ agendaItemTime }}</div>
+      <div class="agenda-item__col">
+        <h3 class="agenda-item__title">{{ agendaItemDefaultTitlesType }}</h3>
+        <p class="agenda-item__talk">
+          <span>{{ agendaItem.speaker }}</span>
+          <span class="agenda-item__dot"></span>
+          <span class="agenda-item__lang">{{ agendaItem.language }}</span>
+        </p>
+        <p>{{ agendaItem.description }}</p>
+      </div>
+    </div>`,
+});
--- 02-components/07-PageMeetup/components/PageMeetup.js
+++ 02-components/07-PageMeetup/components/PageMeetup.js
@@ -1,26 +1,59 @@
 import { defineComponent } from '../vendor/vue.esm-browser.js';
 import UiContainer from './UiContainer.js';
 import UiAlert from './UiAlert.js';
-// import { fetchMeetupById } from './meetupService.js';
+import {fetchMeetupById} from '../meetupService';
+import MeetupView from "./MeetupView";
+
 
 export default defineComponent({
   name: 'PageMeetup',
 
+  data() {
+    return {
+      meetup: null,
+    };
+  },
+
+  props: {
+    meetupId: {
+      type: Number,
+      required: true,
+    }
+  },
+
   components: {
     UiAlert,
     UiContainer,
+    MeetupView,
+  },
+
+  async mounted() {
+    return this.meetup = await fetchMeetupById(this.meetupId)
+      .catch(error => {
+        this.error = error.message;
+      });
+  },
+
+  watch:{
+    async meetupId(meetupId){
+      this.meetup = null;
+      this.meetup = await fetchMeetupById(meetupId)
+        .catch(error => {
+          this.error = error.message;
+        });
+      },
   },
 
   template: `
     <div class="page-meetup">
-      <!-- meetup view -->
+      <MeetupView v-if="meetup" :meetup="meetup" />
 
-      <UiContainer>
+      <UiContainer v-else-if="meetup === null" >
         <UiAlert>Загрузка...</UiAlert>
       </UiContainer>
 
-      <UiContainer>
-        <UiAlert>error</UiAlert>
+      <UiContainer v-else-if="error" >
+        <UiAlert>{{ error }}</UiAlert>
       </UiContainer>
     </div>`,
 });
