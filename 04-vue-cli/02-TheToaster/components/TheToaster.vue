c49a55ad7666b94b41d899a617dae16ab69c4b19
inkoff <kob87@list.ru>
2023-09-22 11:14:16 +0300

    Решение TheToaster c тестами

--- 04-vue-cli/02-TheToaster/components/TheToaster.vue
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -5,13 +5,12 @@
 </template>
 
 <script>
-import UiIcon from './UiIcon.vue';
 import UIToast from './UiToast.vue';
 
 export default {
   name: 'TheToaster',
 
-  components: { UIToast, UiIcon },
+  components: { UIToast, },
 
   data() {
     return {
@@ -57,35 +56,5 @@ export default {
     bottom: 72px;
     right: 112px;
   }
-}
-
-.toast {
-  display: flex;
-  flex: 0 0 auto;
-  flex-direction: row;
-  align-items: center;
-  padding: 16px;
-  background: #ffffff;
-  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
-  border-radius: 4px;
-  font-size: 18px;
-  line-height: 28px;
-  width: auto;
-}
-
-.toast + .toast {
-  margin-top: 20px;
-}
-
-.toast__icon {
-  margin-right: 12px;
-}
-
-.toast.toast_success {
-  color: var(--green);
-}
-
-.toast.toast_error {
-  color: var(--red);
 }
 </style>
--- 04-vue-cli/02-TheToaster/components/UiToast.vue
+++ 04-vue-cli/02-TheToaster/components/UiToast.vue
@@ -10,14 +10,55 @@ export default {
       required: true,
     },
   },
+  computed: {
+    message() {
+      return this.toast.message;
+    },
+    toastClass() {
+      return `toast toast_${this.toast.type}`;
+    },
+    iconClass() {
+      return `${this.toast.type === 'success' ? 'check-circle' : 'alert-circle'}`;
+    },
+  },
 };
 </script>
 
 <template>
-  <div>
-    <!--    <UiIcon />-->
+  <div class="toast" :class="toastClass">
+        <UiIcon :icon="iconClass"/>
     <span>{{ toast.message }}</span>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+</style>
