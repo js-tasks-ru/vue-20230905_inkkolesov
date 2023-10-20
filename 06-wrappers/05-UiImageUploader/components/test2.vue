e715b1cdb217511dc04796727ac9c7f5e741eead
inkoff <kob872list.ru>
2023-10-20 09:10:35 +0300

    решение UiImageUploader.vue - click

--- 06-wrappers/05-UiImageUploader/components/test2.vue
+++ 06-wrappers/05-UiImageUploader/components/test2.vue
@@ -2,7 +2,7 @@
   <div class="image-uploader">
     <label
       class="image-uploader__preview"
-      :class="{ 'image-uploader__preview-loading': !preview && uploader }"
+      :class="{ 'image-uploader__preview-loading': !preview && uploader && !hasUploader }"
       :style="previewStyle"
     >
       <span class="image-uploader__text">{{ text }}</span>
@@ -96,49 +96,5 @@ export default {
     },
   },
   emits: ['select', 'upload', 'remove', 'error'],
-  expose: [],
 };
 </script>
-
-<style scoped>
-.image-uploader {
-}
-
-.image-uploader__input {
-  opacity: 0;
-  height: 0;
-}
-
-.image-uploader__preview {
-  --bg-url: var(--default-cover);
-  background-size: cover;
-  background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--bg-url);
-  border: 2px solid var(--blue-light);
-  border-radius: 8px;
-  transition: 0.2s border-color;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  max-width: 512px;
-  height: 228px;
-}
-
-.image-uploader__text {
-  color: var(--white);
-  font-family: 'Nunito', sans-serif;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 28px;
-}
-
-.image-uploader__preview:hover {
-  border-color: var(--blue);
-}
-
-.image-uploader__preview.image-uploader__preview-loading {
-  cursor: no-drop;
-}
-</style>
