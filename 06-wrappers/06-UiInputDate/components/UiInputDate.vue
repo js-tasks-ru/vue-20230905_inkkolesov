965b16d11e460e021c8dd519bd4fc316c37e40e8
inkoff <kob872list.ru>
2023-10-21 16:40:19 +0300

    решение UiInputDate.vue - рефакторинг

--- 06-wrappers/06-UiInputDate/components/UiInputDate.vue
+++ 06-wrappers/06-UiInputDate/components/UiInputDate.vue
@@ -1,13 +1,51 @@
 <template>
-  <UiInput />
+  <UiInput :type="type" :step="step" v-model="modelValueProxy" @input="onInput">
+    <template #left-icon>
+      <slot name="left-icon" />
+    </template>
+  </UiInput>
 </template>
 
 <script>
-import UiInput from './UiInput.vue';
+import UiInput from './UiInput.vue'; // Замените на путь к вашему компоненту UiInput
 
 export default {
+  components: {
+    UiInput,
+  },
   name: 'UiInputDate',
-
-  components: { UiInput },
+  props: {
+    type: {
+      type: String,
+      default: 'date',
+      validator: (value) => ['date', 'time', 'datetime-local'].includes(value),
+    },
+    modelValue: {
+      type: [Number, null],
+      default: null,
+    },
+    step: String,
+  },
+  computed: {
+    dateISO() {
+      return new Date(this.modelValue).toISOString();
+    },
+    localDate() {
+      return {
+        date: this.dateISO.substring(0, 10),
+        time: this.dateISO.substring(11, 16),
+        'datetime-local': this.dateISO.substring(0, 16),
+      };
+    },
+    modelValueProxy() {
+      return this.modelValue ? this.localDate[this.type] : '';
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('update:modelValue', event.target.valueAsNumber);
+    },
+  },
+  emits: ['update:modelValue'],
 };
 </script>
