ecf4a28784f5ed3794599011c4e81aed560eab12
inkoff <kob872list.ru>
2023-11-12 17:19:32 +0300

    реактивность. потеря реактивности - решение с reactify.js

---
+++ 08-reactivity/05-reactify/utils/reactify.js
@@ -0,0 +1,13 @@
+import { computed, reactive, ref } from 'vue';
+
+/**
+ * @template T
+ * @param {function(...[*]): T} func - Исходная функция вычисления
+ * @returns {function(...[*]): ComputedRef<T>} - Функция вычисления от ref-ов, возвращающая вычисляемое значение computed
+ */
+export function reactify(func) {
+  return (...args) =>
+    computed(() => {
+      return func(...args.map((arg) => (arg.value !== undefined ? arg.value : ref(arg).value)));
+    });
+}
