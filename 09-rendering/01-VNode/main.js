823eae4a88efb1601ee04bf26afb05b6294d6a47
inkoff <kob872list.ru>
2023-11-12 19:48:01 +0300

    реактивность. решение с reactify.js после ревью

--- 09-rendering/01-VNode/main.js
+++
@@ -1,8 +0,0 @@
-import '@/assets/styles/taskbook.css';
-import '@/assets/styles/app.css';
-import '@/assets/styles/_container.css';
-
-import { createApp } from 'vue';
-import App from './App.vue';
-
-createApp(App).mount('#app');
